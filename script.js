c30f75bab2bddeec769df3fa51b1d4531802813e
Shihai <80754795+MohimnMajid@users.noreply.github.com>
2024-07-12 04:35:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let testy = function () {
+  return Swal.fire({
+    title: "نجاح",
+    text: "لقد تم وضع طلبك بنجاح",
+    icon: "success",
+    confirmButtonText: "اكمل",
+  });
+};
